b788e34801f8b0d6abd9b84ffbb0e6c0217f4d2f
FaizFerozMehmood <ferozfaaiz5@gmail.com>
2024-07-23 16:19:58 +0500

    done

--- app.js
+++ app.js
@@ -12,18 +12,16 @@ const plusBtn = document.querySelector('.plus')
 const minusBtn = document.querySelector('.minus')
 const number = document.querySelector('.number')
 counter = 0;
-minusBtn.Disabled=false
 
 
 plusBtn.addEventListener('click',function(){
   counter++
   number.innerText = counter
-
+  
 })
 minusBtn.addEventListener('click',function(){
   counter--
   number.innerText=counter
-  if(counter==="0"){
-minusBtn.Disabled=true
-  }
+  
+ 
 })
