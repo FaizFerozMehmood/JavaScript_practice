4f92ff763ba4dd0d2cfa7ca9bc33cc521bf578cb
FaizFerozMehmood <ferozfaaiz5@gmail.com>
2024-07-23 16:06:17 +0500

    counter app

--- app.js
+++ app.js
@@ -1,10 +1,29 @@
-function myFirst() {
-    myDisplayer("Hello");
-  }
+// function myFirst() {
+//     myDisplayer("Hello");
+//   }
   
-  function mySecond() {
-    myDisplayer("Goodbye");
-  }
+//   function mySecond() {
+//     myDisplayer("Goodbye");
+//   }
   
-  myFirst();
-  mySecond();
+//   myFirst();
+//   mySecond();
+const plusBtn = document.querySelector('.plus')
+const minusBtn = document.querySelector('.minus')
+const number = document.querySelector('.number')
+counter = 0;
+minusBtn.Disabled=false
+
+
+plusBtn.addEventListener('click',function(){
+  counter++
+  number.innerText = counter
+
+})
+minusBtn.addEventListener('click',function(){
+  counter--
+  number.innerText=counter
+  if(counter==="0"){
+minusBtn.Disabled=true
+  }
+})
