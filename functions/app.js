e66b32e2f250697033b1271e51562ec15be8113d
FaizFerozMehmood <ferozfaaiz5@gmail.com>
2024-07-28 04:36:43 +0500

    working on function

--- functions/app.js
+++ functions/app.js
@@ -1,5 +1,22 @@
-function sayHi(username){
-   console.log(`i am ${username}`);
+// function sayHi(username){
+// //    console.log(`i am ${username}`);
+// const 
+// }
+// sayHi("faiz")
+// sayHi("umair")
+// function sayHi(greeting,username){
+//     const user = `${greeting}`
+//     const userNms= `${username}`
+//       console.log(user,userNms);
+//     }
+// sayHi("HI!","faiz")
+function introducteMe(user,age,proffession){
+    // console.log(`${user}`);
+    // console.log(`${age}`);
+    // console.log(`${proffession}`);
+    return user + age + proffession
+
+
 }
-sayHi("faiz")
-sayHi("umair")
+introducteMe("faiz",21,"developer")
+console.log(introducteMe(1,2,3),introducteMe(1,2,3),introducteMe(1,2,3));
